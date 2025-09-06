e4d6f5697f67b93e6ae18e306f66ce68211b109e
Martin9 <martinjcantillo9@gmail.com>
2025-09-06 09:18:18 -0500

    feat: add interceptor

---
+++ frontend/src/app/interceptor/interceptor.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { MessageService } from 'primeng/api';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private messageService: MessageService) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        const msg = error.error?.message || error.statusText || 'Unknown error';
+        this.messageService.add({ severity: 'error', summary: `HTTP ${error.status}`, detail: msg });
+        return throwError(() => error);
+      })
+    );
+  }
+}
